df7210d0098819a63d09abc3a4408f06f8f9729a
Kakune55 <xiao_doubi9637@outlook.com>
2024-04-21 17:43:26 +0800

    [feat]:为主页添加搜索功能

--- web/page.py
+++ web/page.py
@@ -10,9 +10,9 @@ conf = app_conf.conf()
 @page_bp.route("/overview/<page>")
 def overview(page):  # 概览
     page = int(page)
-    if request.cookies.get("islogin") is None:
-        return redirect("/")
-    metaDataList = db.file.getMetadata((page - 1) * 20, page * 20)
+    if request.cookies.get("islogin") is None: #验证登录状态
+        return redirect("/")   
+    metaDataList = db.file.getMetadata((page - 1) * 20, page * 20, request.args.get("search"))
     for item in metaDataList:
         item[2] = item[2][:-4] #去除文件扩展名
     if page <= 3:
@@ -31,8 +31,8 @@ def overview(page):  # 概览
 
 @page_bp.route("/book/<bookid>")
 def book(bookid):  # 接口
-    if request.cookies.get("islogin") is None:
-        return abort(403)
+    if request.cookies.get("islogin") is None: #验证登录状态
+        return redirect("/")   
     data = db.file.searchByid(bookid)
     if len(data) == 0:
         return abort(404)
@@ -50,8 +50,8 @@ def book(bookid):  # 接口
 
 @page_bp.route("/view/<bookid>")
 def view(bookid):  # 接口
-    if request.cookies.get("islogin") is None:
-        return abort(403)
+    if request.cookies.get("islogin") is None: #验证登录状态
+        return redirect("/")   
     data = db.file.searchByid(bookid)
     if len(data) == 0:
         return abort(404)
@@ -60,6 +60,8 @@ def view(bookid):  # 接口
 
 @page_bp.route("/upload", methods=["GET", "POST"])  # 文件上传
 def upload_file():
+    if request.cookies.get("islogin") is None: #验证登录状态
+        return redirect("/")   
     if request.method == "GET":
         return render_template("upload.html")
     uploaded_file = request.files.getlist("files[]")  # 获取上传的文件列表
